2feaa4a82021e0aa98148d2bd20b7fcafcdf1997
Juan Montemayor <jam1@alum.mit.edu>
2013-07-12 06:32:52 +0000

    Added option to specify URL

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -38,6 +39,10 @@ var assertFileExists = function(infile) {
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var cheerioHtml = function(Html) {
+    return cheerio.load(Html);
 };
 
 var loadChecks = function(checksfile) {
@@ -55,20 +60,45 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtml = function(html, checksfile) {
+    $ = cheerioHtml(html);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var gradeJson = function(htmlfile,url,checksfile) {
+    if (!url) {
+	console.log(JSON.stringify(checkHtmlFile(htmlfile,checksfile), null, 4));
+    } else {
+	rest.get(url).on('complete',function(result) {
+	    if (result instanceof Error) {
+		console.log("Error: %s", result.message);
+		process.exit(1);
+	    } else {
+		console.log(JSON.stringify(checkHtml(result,checksfile), null, 4));
+	    }
+	});
+    }
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .option('-u, --url <url>', 'URL to file')
+        .parse(process.argv);    
+    gradeJson(program.file, program.url, program.checks);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
